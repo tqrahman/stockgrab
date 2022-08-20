6a6f75d733785886b8f06fc17fe577db23abfebb
Taraqur <tqrahman8@gmail.com>
2022-08-20 17:56:53 -0400

    some css for forms

--- stockgrab/home/forms.py
+++ stockgrab/home/forms.py
@@ -2,7 +2,15 @@
 from django import forms
 
 class StockForm(forms.Form):
-    tick = forms.CharField(required=True)
-    start_date = forms.DateField(required=False)
-    end_date = forms.DateField(required=False)
-    interval = forms.CharField(required=False)
+
+    # Creating a list of possible interval choices
+    interval_choices = [
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+        ('monthly', 'Monthly')
+    ]
+
+    tick = forms.CharField(label='Tick Symbol', required=True)
+    start_date = forms.DateField(label='Start Date', required=False)
+    end_date = forms.DateField(label='End Date', required=False)
+    interval = forms.CharField(label='Frequency', required=False, widget=forms.Select(choices=interval_choices))
