79a222cf7ef0542076539fbcdf386f148e63fc42
Taraqur <tqrahman8@gmail.com>
2022-09-06 00:08:34 -0400

    cleaned code a bit

--- stockgrab/home/views.py
+++ stockgrab/home/views.py
@@ -1,6 +1,12 @@
 # Imports
-from pickle import GLOBAL
+
+import gzip
+from http.client import HTTPResponse
+from io import BytesIO
 from django.shortcuts import render
+import csv
+from django.utils.encoding import smart_str
+
 from .forms import StockForm
 from .scrape import convert_to_dataframe, convert_to_unix_time, get_query, get_data
 
@@ -11,38 +17,43 @@ def home_page(request):
     # Checking to see if user submitted information
     if request.method=='POST':
         
-
         # Extracting the tick symbol from input
-        
         tick = request.POST['tick']
-        #tick_read = requests.get(tick,headers={"User-Agent": ""} )
 
         # Extracting the start_date from input
-        start_date = convert_to_unix_time(request.POST['start_date'])
-        
+        start_date = request.POST['start_date']
+        start_unix = convert_to_unix_time(start_date)
 
         # Extracting the end_date from input
         end_date = request.POST['end_date']
-        end_date = convert_to_unix_time(end_date)
-        # end_date_read = requests.get(end_date,headers={"User-Agent": ""})
+        end_unix = convert_to_unix_time(end_date)
 
         # Extracting the interval from input
         interval = request.POST['interval']
-        # interval_read = requests.get(interval,headers={"User-Agent": ""} )
-
-
         
-        # Sanity check
-        #user_input = f"I need stock {tick} from {start_date} - {end_date} every {interval}"
-        url = get_query(tick,start_date,end_date,interval) 
-        #jsonStockData = json.loads(obtain.text)
-        #print(f"I need stock {tick} from {start_date} - {end_date} every {interval}")
+        # Creating user input
+        user_input = f"Grabbed the Stock! \n Here is the data for {tick} from {start_date}-{end_date} every {interval}"
+        
+        # Get URL given the user inputs 
+        url = get_query(tick, start_unix, end_unix, interval) 
 
-        #print(url)
-
+        # Get the data
         list_data = get_data(url)
-        df_convert = convert_to_dataframe(list_data) 
-        print(df_convert.head())
+        
+        # Convert list of data into a pandas Dataframe
+        df = convert_to_dataframe(list_data) 
+        # print(df.head())
+
+        # Download the data
+        # b = BytesIO()
+        # with gzip.open(b, 'wb') as f:
+        #     f.write(df.to_csv().encode())
+        # response = HTTPResponse(b.getvalue(), content_type='text/csv')
+        # response['Content-Disposition'] = 'attachment; filename="stockgrab_data.csv"'
+
+        # response = HTTPResponse(open("uploads/something.txt", 'rb').read())
+        # response['Content-Type'] = 'text/plain'
+        # response['Content-Disposition'] = 'attachment; filename=DownloadedText.txt'
 
         return render(request, 'home/results.html', context={'user_input':user_input})
     
